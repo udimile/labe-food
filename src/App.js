deb99c1d33d67116c33ccd3efa4a7fc932120cd9
udimile <76756821+udimile@users.noreply.github.com>
2022-05-17 14:02:21 -0300

    Copiando repositório usado no trabalho em grupo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ThemeProvider } from '@mui/material';
+import React from 'react';
+import theme from './constants/theme';
+import GlobalState from './global/GlobalState';
+import Router from './routes/Router';
+
+function App() {
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalState>
+        <Router />
+      </GlobalState>
+    </ThemeProvider>
+  );
+}
+
+export default App;
